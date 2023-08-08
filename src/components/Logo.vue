fbb8cca965354e92cfcdc97792291cae3960b1d9
iozhaq <iozhaq@outlook.com>
2023-08-08 10:31:51 +0800

    header logo 尺寸修改,

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <strong class="logo block w-52 md:w-38 flex-shrink-0">
     <router-link to="/overview">
-      <img src="/assets/favicon.png" alt="CT Wallet">
+      <img src="/assets/favicon.png" alt="CT Wallet" style="height: 28px">
     </router-link>
   </strong>
 </template>
